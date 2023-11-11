05d141cac992448dfe89c6cc979f09c314b0122f
Aaron Ellington <aaron@ellington.io>
2023-11-11 13:42:57 -0500

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import '@aaronellington/reset-css'
 
 import Button from './components/Simple/Button'
 import Card from './components/Simple/Card'
-import LayoutStandard from './components/Layout/LayoutStandard'
-import LayoutMenu from './components/Layout/LayoutMenu'
+import LayoutCarbon from './components/Layout/LayoutCarbon'
+import LayoutHydrogen from './components/Layout/LayoutHydrogen'
+import LayoutSilicon from './components/Layout/LayoutSilicon'
+import LayoutSodium from './components/Layout/LayoutSodium'
 
-export { Button, Card, LayoutMenu, LayoutStandard }
+export {
+	Button,
+	Card,
+	LayoutCarbon,
+	LayoutHydrogen,
+	LayoutSilicon,
+	LayoutSodium,
+}
